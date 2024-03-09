7978d66f01586c4dcd5f5e286daf0089e155a757
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-03-09 11:14:51 +0100

    Add typeScript() to transpile to commonJs

---
+++ test/find.test.ts
@@ -0,0 +1,47 @@
+import {suite, test} from "mocha";
+import assert from "node:assert";
+import process from "node:process";
+
+import {find} from "../src/find.ts";
+import {directory} from "./fixture/directory.ts";
+
+suite('distribute()', () => {
+  suite('find/', () => {
+    test('empty directory', () =>
+      directory(dir =>
+        assert.deepEqual(find(dir.path), [])));
+
+    test('files in directory', () =>
+      directory(dir => {
+        dir.write('foo', '');
+        dir.write('bar', '');
+        assert.deepEqual(find(dir.path), ['bar', 'foo']);
+      }));
+
+    test('ignore directory', () =>
+      directory(dir => {
+        dir.write('foo', '');
+        dir.create('bar');
+        assert.deepEqual(find(dir.path), ['foo']);
+      }));
+
+    test('file in child directory', () =>
+      directory(dir => {
+        dir.write('foo/file.txt', '');
+        assert.deepEqual(find(dir.path), [expected('foo', 'file.txt')]);
+      }));
+
+    test('file in nested directories', () =>
+      directory(dir => {
+        dir.write('foo/bar/file.txt', '');
+        assert.deepEqual(find(dir.path), [expected('foo', 'bar', 'file.txt')]);
+      }));
+  });
+
+  function expected(...paths: string[]): string {
+    if (process.platform === 'win32') {
+      return paths.join("\\");
+    }
+    return paths.join('/');
+  }
+});
