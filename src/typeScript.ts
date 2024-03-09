cc899721f7e7d98cf53b93f393031e5bc68b6fda
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-03-09 13:21:26 +0100

    typeScript() updates import extension to .js

--- src/typeScript.ts
+++ src/typeScript.ts
@@ -1,16 +1,17 @@
 import fs from "node:fs";
 import {dirname, join} from "node:path";
-import {ModuleKind, transpile} from "typescript";
+import {type CompilerOptions, ModuleKind, transpileModule, type TranspileOutput} from "typescript";
 
 import {find} from "./find.ts";
 import {type Operation, packageJson, read} from "./package.ts";
+import {updateImport} from "./updateImport.ts";
 
 export function typeScript(root: string, main: string): Operation {
   return (output: string): void => {
     find(root).forEach(file => {
       writeFile(
         join(output, 'dist', 'cjs', js(file)),
-        transpile(read(join(root, file))));
+        transpile(read(join(root, file)), {}));
       writeFile(
         join(output, 'dist', 'esm', js(file)),
         transpile(read(join(root, file)), {module: ModuleKind.ES2015}));
@@ -20,6 +21,14 @@ export function typeScript(root: string, main: string): Operation {
       module: "./dist/esm/" + js(main),
     })(output);
   };
+}
+
+function transpile(sourceCode: string, compilerOptions: CompilerOptions): string {
+  const output: TranspileOutput = transpileModule(sourceCode, {
+    compilerOptions,
+    transformers: {after: [updateImport]},
+  });
+  return output.outputText;
 }
 
 function js(file: string): string {
