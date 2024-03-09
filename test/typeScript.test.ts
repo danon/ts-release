d6501a720d34c5afc9afcae285a6128623cfbb61
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-03-09 13:21:26 +0100

    typeScript() updates import extension to .js

--- test/typeScript.test.ts
+++ test/typeScript.test.ts
@@ -3,19 +3,19 @@ import {strict as assert} from "node:assert";
 
 import {distribute} from "../src/package.ts";
 import {typeScript} from "../src/typeScript.ts";
-import {assertOutputFilename, assertTranspile} from "./fixture/assert.ts";
+import {assertOutputFilename, assertTranspile, assertTranspileIgnore, assertTranspileTarget} from "./fixture/assert.ts";
 import {directory} from "./fixture/directory.ts";
 
 suite('distribute()', () => {
   suite('typeScript()', () => {
     suite('target', () => {
       test('transpile to commonJs',
-        assertTranspile('cjs', 'import "node:fs";', `"use strict";
+        assertTranspileTarget('cjs', 'import "node:fs";', `"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("node:fs");`));
 
       test('transpile to esModule',
-        assertTranspile('esm', 'import "node:fs";', 'import "node:fs";'));
+        assertTranspileTarget('esm', 'import "node:fs";', 'import "node:fs";'));
     });
 
     test('.ts.ts',
@@ -35,5 +35,19 @@ require("node:fs");`));
           module: './dist/esm/nest/foo.js',
         });
       }));
+
+    suite('extension', () => {
+      test('.ts to .js',
+        assertTranspile('import "./file.ts";', 'import "./file.js";'));
+
+      test('export',
+        assertTranspileIgnore('export * from "./file.ts";'));
+
+      test('trailing .ts',
+        assertTranspile('import "./file.ts.ts";', 'import "./file.ts.js";'));
+
+      test('not extension',
+        assertTranspile('import "ats";', 'import "ats";'));
+    });
   });
 });
